ea139689b8dc1d7b4cea7f37da4959fc56bc70ad
denev66 <kevin.velez661@gmail.com>
2020-02-08 12:20:42 -0500

    Movement System with Jump and animations

--- Assets/Scripts/oldMovement.cs
+++ Assets/Scripts/oldMovement.cs
@@ -4,33 +4,48 @@ using UnityEngine;
 
 public class oldMovement : MonoBehaviour
 {
-    public float strength = 50;
+    public float speed = 10;
     Rigidbody rigidbody;
+    Animator myAnimator;
+
+    private Vector3 inputVector;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = this.GetComponent<Rigidbody>();
+        myAnimator = this.GetComponent<Animator>();
     }
 
-
-    void FixedUpdate()
-    {
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rigidbody.AddForce(transform.forward * strength, ForceMode.VelocityChange); // mass does not affect
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rigidbody.AddForce((transform.forward*-1) * strength, ForceMode.VelocityChange); // mass does not affect
-        }
-       
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        myAnimator.SetFloat("distanceToTarget", horizontal);
+
+        print(myAnimator.GetFloat("distanceToTarget"));
+        Vector3 movement = new Vector3(horizontal* speed *Time.deltaTime, 0, vertical * speed *Time.deltaTime);
+        rigidbody.MovePosition(transform.position + movement);
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidbody.AddForce(transform.up * 9.5f, ForceMode.Impulse); // mass affects
+            print("Holi soy el salto");
+        }
+
     }
+
+
+
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(Physics.gravity * (rigidbody.mass + 1));
+        //rigidbody.velocity = inputVector*2;       
+    }
+
+
 }
