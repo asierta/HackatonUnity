3a5c257b0784dd98126111f3d1a780006aefeeff
denev66 <kevin.velez661@gmail.com>
2020-02-08 09:48:08 -0500

    RigidBodie movement

---
+++ Assets/Scripts/oldMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oldMovement : MonoBehaviour
+{
+    public float strength = 50;
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody.AddForce(transform.forward * strength, ForceMode.VelocityChange); // mass does not affect
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody.AddForce((transform.forward*-1) * strength, ForceMode.VelocityChange); // mass does not affect
+        }
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
