a83b32e0436f9773f262864b5550ce39c7075335
TaiPrev <taiprev@gmail.com>
2020-02-11 23:30:20 +0100

    prepare for player input
    Procedural world is getting a pendulum player

---
+++ Assets/Scripts/PlayerDangler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerDangler : MonoBehaviour
+{
+    private Vector2 mousePos;
+
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Get the angle of the mouse regarding the player
+        float angle = Mathf.Atan2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+
+        gameObject.transform.Rotate(Vector3.up, angle);
+    }
+}
